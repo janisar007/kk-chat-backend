2be5847d1e360d7a070ef424c27be452dc070c5d
Balkishun Maurya <140256426+Bkishun@users.noreply.github.com>
2024-09-22 20:03:27 +0530

    socketio

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-import express from "express"
+import express from "express";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import cors from "cors";
+import { createServer } from "http"; // To create an HTTP server
+import { Server } from "socket.io"; // Import socket.io
 
-import cors from "cors"
-
-import userRoutes from "./routes/user.route.js"
-import chatRoutes from "./routes/chat.route.js"
-import messageRoutes from "./routes/message.route.js"
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { errorHandler, notFound } from "./middlewares/error.middleware.js";
 
-dotenv.config()
-connectDB()
+dotenv.config();
+connectDB();
+
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-}))
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use(express.json());
 
@@ -26,8 +30,42 @@ app.use("/api/message", messageRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
+// Create an HTTP server
+const server = createServer(app);
 
+// Initialize socket.io
+const io = new Server(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+    // credentials: true,
+  },
+});
 
+io.on("connection", (socket) => {
+    console.log("Connected to socket.io");
+    socket.on("setup", (userData) => {
+      socket.join(userData._id);
+      socket.emit("connected");
+    });
 
-app.listen(3001, () => console.log("listening on 3001..."))
+    socket.on("join chat", (room) => {
+        socket.join(room);
+        console.log("User Joined Room: " + room);
+    });
 
+    socket.on("new message", (newMessageRecieved) => {
+        var chat = newMessageRecieved.chat;
+    
+        if (!chat.users) return console.log("chat.users not defined");
+    
+        chat.users.forEach((user) => {
+          if (user._id == newMessageRecieved.sender._id) return;
+    
+          socket.in(user._id).emit("message recieved", newMessageRecieved);
+        });
+    });
+});
+
+// Start the server
+server.listen(3001, () => console.log("Server listening on port 3001..."));
